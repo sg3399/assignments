c0832c30398872d3ddb5f2ebf02e5e5818dc16b1
Siddhant <sg3399@columbia.edu>
2020-01-11 20:45:18 -0500

    created basic math functions in java and added a line explaining it

---
+++ homework_2/calculator.js
@@ -0,0 +1,26 @@
+//his script makes functions for basic math operations
+
+//This defines a function that takes two inputs: a and b, and returns a times b  
+function multiply(a,b){
+    return a * b
+}
+
+//This defines a function that takes two inputs: a and b, and returns a plus b  
+function add(a,b){
+    return a + b
+}
+
+//This defines a function that takes two inputs: a and b, and returns a minus b  
+function subtract(a,b){
+    return a - b
+}
+
+//This defines a function that takes two inputs: a and b, and returns a divded by b  
+function divide(a,b){
+    return a / b
+}
+
+console.log("I'm going use the calculator functions to divide 5 and 6")
+var x = divide(5,6)
+console.log(x)  
+
