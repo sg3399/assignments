058ff1eacb1b07550ae7cb2994fa7b47f5eafd43
Siddhant <sg3399@columbia.edu>
2020-01-11 19:53:03 -0500

    call divide function and printing a line explaining it

--- calculator.py
+++ calculator.py
@@ -14,9 +14,9 @@ def subtract(a,b):
 
 #This defines a function that takes two inputs: a and b, and returns a divded by b  
 def divide(a,b):
-    return a // b
+    return a / b
 
-print("I'm going use the calculator functions to add 5 and 6")
-x = add(5,6)
+print("I'm going use the calculator functions to divide 5 and 6")
+x = divide(5,6)
 print(x)  
 
