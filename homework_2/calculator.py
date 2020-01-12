cefc14be9ecf0448d0fbcb037643a789007b8686
Siddhant <sg3399@columbia.edu>
2020-01-11 20:36:15 -0500

    adding bonus parts

--- homework_2/calculator.py
+++ homework_2/calculator.py
@@ -16,7 +16,33 @@ def subtract(a,b):
 def divide(a,b):
     return a / b
 
+#This defines a function that takes an inputs: a , and returns a squared 
+def square(a):
+    return a ** 2
+
+#This defines a function that takes an inputs: a , and returns a cubed 
+def cube(a):
+    return a ** 3
+
+#This defines a function that takes an inputs: a and n, and returns 'a' squared n times.
+def square_n(a,n):
+    return a ** (2**n)
+
+
+
 print("I'm going use the calculator functions to divide 5 and 6")
-x = divide(5,6)
+w = divide(5,6)
+print(w)  
+
+print("I'm going use the calculator functions to square 2")
+x = square(2)
 print(x)  
+
+print("I'm going use the calculator functions to cube 2")
+y = cube(2)
+print(y) 
+
+print("I'm going use the calculator functions to square 2 three times")
+z = square_n(2,3)
+print(z) 
 
